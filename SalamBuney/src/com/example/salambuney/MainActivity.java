bf2c238dc99abab4d10c601497400444c22bb68d
m.nasru <m.nasru@gmail.com>
2012-12-20 04:41:51 +0000

    manifest updated to give permission to read and write to sms now includes logic to check whether sms was sent and if so write to sent messages

--- SalamBuney/src/com/example/salambuney/MainActivity.java
+++ SalamBuney/src/com/example/salambuney/MainActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.telephony.SmsManager;
 import android.view.Menu;
@@ -60,17 +65,70 @@ public class MainActivity extends Activity {
 
 		SharedPreferences sharedPref = PreferenceManager
 				.getDefaultSharedPreferences(this);
+
+		String smsNumber = sharedPref.getString("mobile", "not set");
+		String smsText = sharedPref.getString("message", "not set");
+
+		/*
+		 * Uri uri = Uri.parse("smsto:" + smsNumber); Intent intent = new
+		 * Intent(Intent.ACTION_SEND, uri); intent.putExtra("sms_body",
+		 * smsText); startActivity(intent);
+		 */
+
+		String SENT = "SMS_SENT";
+
+		PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(
+				SENT), 0);
+
+		// ---when the SMS has been sent---
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS sent",
+							Toast.LENGTH_SHORT).show();					
+					StoreSMS ();
+
+					break;
+				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+					Toast.makeText(getBaseContext(), "Generic failure",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_NO_SERVICE:
+					Toast.makeText(getBaseContext(), "No service",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_NULL_PDU:
+					Toast.makeText(getBaseContext(), "Null PDU",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_RADIO_OFF:
+					Toast.makeText(getBaseContext(), "Radio off",
+							Toast.LENGTH_SHORT).show();
+					break;
+				}
+			}
+		}, new IntentFilter(SENT));
+
+		// send sms
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(smsNumber, null, smsText, sentPI, null);
+		
+	}
+	
+	public void StoreSMS ()
+	{
+		SharedPreferences sharedPref = PreferenceManager
+				.getDefaultSharedPreferences(this);
 		
 		String smsNumber = sharedPref.getString("mobile", "not set");
-	    String smsText = sharedPref.getString("message", "not set");
-	    
-	    /*Uri uri = Uri.parse("smsto:" + smsNumber);
-	    Intent intent = new Intent(Intent.ACTION_SEND, uri);
-	    intent.putExtra("sms_body", smsText);  
-	    startActivity(intent);*/
-	    
-	    SmsManager sms = SmsManager.getDefault();
-	    sms.sendTextMessage(smsNumber, null, smsText, null, null);
-	    
+		String smsText = sharedPref.getString("message", "not set");
+		
+		// write to sent messages
+		ContentValues values = new ContentValues();
+		values.put("address", smsNumber);
+		values.put("body", smsText);
+		getContentResolver().insert(Uri.parse("content://sms/sent"), values);
 	}
 }
